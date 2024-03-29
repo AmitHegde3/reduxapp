7874de89b452b126096d4ead03c0a6dd06453e52
Amit Hegde <amit.is21@bmsce.ac.in>
2024-03-29 15:45:52 +0530

    Create React APP

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+      <div>
+          <h2>Buy Abibas Shoes at 50Rs.</h2>
+      <button className="btn btn-primary mx-2">-</button>
+      Buy this Item
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  );
+};
+
+export default Shop;
